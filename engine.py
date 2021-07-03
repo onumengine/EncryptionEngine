fbc92b71529bd49516da331e58feaade3c23cdac
onumdev <onumdev@gmail.com>
2021-07-03 01:13:40 +0100

    update the encrypt method

--- engine.py
+++ engine.py
@@ -1,51 +1,22 @@
-encoding_data = {
-    "a": "z",
-    "b": "y",
-    "c": "x",
-    "d": "w",
-    "e": "v",
-    "f": "u",
-    "g": "t",
-    "h": "s",
-    "i": "r",
-    "j": "q",
-    "k": "p",
-    "l": "o",
-    "m": "n",
-    "n": "m",
-    "o": "l",
-    "p": "k",
-    "q": "j",
-    "r": "i",
-    "s": "h",
-    "t": "g",
-    "u": "f",
-    "v": "e",
-    "w": "d",
-    "x": "c",
-    "y": "b",
-    "z": "a"
-}
+from encoding_data import string_map
 
 def get_encoded_string(text):
 
-    length = len(text)
-    string_array = []
+    temp_string = ""
 
-    for i in range(length):
-        current_letter = text[i]
-        if current_letter in encoding_data:
-            string_array.append(encoding_data[current_letter])
-        elif current_letter.lower() in encoding_data:
-            string_array.append(encoding_data[current_letter.lower()].upper())
+    for letter in text:
+        current_letter = letter
+        if current_letter in string_map:
+            temp_string += (string_map[current_letter])
+        elif current_letter.lower() in string_map:
+            temp_string += (string_map[current_letter.lower()].upper())
         elif current_letter == " ":
-            string_array.append("_")
+            temp_string += ("_")
         else:
             continue
     
-    new_string = str(string_array)
-    refined_string = new_string.replace("'", "").replace(",", "").replace("[", "").replace("]", "").replace(" ", "").replace("_", " ")
-    return refined_string
+    encoded_string = temp_string.replace("'", "").replace(",", "").replace("[", "").replace("]", "").replace(" ", "").replace("_", " ")
+    return encoded_string
 
 def run_script():
     unencoded_string = input("Enter a string: ")
